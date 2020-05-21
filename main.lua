6c8f3f9740e959bed8554951694c6c46f976282d
RIPHUB101XD <65584109+RIPHUB101XD@users.noreply.github.com>
2020-05-20 21:00:53 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
+print(_G.WHITELIST_KEY)
 local KEY_RUHEIURHEIR = _G.WHITELIST_KEY
 local DATABASEURL = "https://sheets.googleapis.com/v4/spreadsheets/1T3ADvsjd_TQuh859GujOMHcDKnDXHhVm9xtD89-gc6w/values/Sheet1!C1:C10000?key=AIzaSyDLdFilXrAeI8sZrLJwEzCXoX4fAIGXFcQ"
-print("DM ME ON DISCORD IF U SEE THIS")
 
 response = game:HttpGet(DATABASEURL , true)
 data = game.HttpService:JSONDecode(response)
