661d794f65a5eeb2269c9bae0f926c218e444ed7
RIPHUB101XD <65584109+RIPHUB101XD@users.noreply.github.com>
2020-05-20 20:54:59 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,19 @@
-print("XD")
+local KEY_RUHEIURHEIR = _G.WHITELIST_KEY
+local DATABASEURL = "https://sheets.googleapis.com/v4/spreadsheets/1T3ADvsjd_TQuh859GujOMHcDKnDXHhVm9xtD89-gc6w/values/Sheet1!C1:C10000?key=AIzaSyDLdFilXrAeI8sZrLJwEzCXoX4fAIGXFcQ"
+
+ response = game:HttpGet(DATABASEURL , true)
+ data = game.HttpService:JSONDecode(response)
+
+function CheckKey()
+  for _, a in pairs(unpack(data.values)) do
+    if KEY_RUHEIURHEIR == a then
+       return true
+    else
+       return false
+    end
+end
+
+  
+ if (CheckKey() == true) then
+    print("XD")
+ end
